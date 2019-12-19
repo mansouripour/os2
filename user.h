b4386769ae902311555dc2eaf3d1eaa6c02e1d6f
monamansouri <36542241+monamansouri@users.noreply.github.com>
2019-12-19 15:52:58 +0330

    Update user.h

--- user.h
+++ user.h
@@ -23,6 +23,9 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+//
+int getChild(void);//void????int????
+int getCount(void);//
 
 // ulib.c
 int stat(const char*, struct stat*);
