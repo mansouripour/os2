f7f8665b5b8b2a01000fe629f6de3ea7e2f8f11e
f mansouri <36542241+monamansouri@users.noreply.github.com>
2019-12-21 02:35:01 +0330

    Update defs.h

--- defs.h
+++ defs.h
@@ -120,6 +120,8 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
+int 		        getChild(int);
+int 		        getCount(int);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
